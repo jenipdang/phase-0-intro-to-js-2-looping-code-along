1547c9acff31c117183d2438c8b5a935d5044c43
Jeni Dang <jenipdang@yahoo.com>
2021-09-17 19:46:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+/*for (let age =30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(["Ada", "Brendan", "Alii"], "birthday");
+
+function countDown(number) {
+    while (number >= 0) [
+        console.log(number--)
+    ]
+}
+
+countDown(10);
